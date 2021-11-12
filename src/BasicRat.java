734a5c3be6144b68bf0782c3e06d2d39ed15cee6
AndrewS9044 <2007157@swansea.ac.uk>
2021-11-12 15:39:48 +0000

    updated death/basic rat
    will need to be updated again once tilemanager/ratmanger are implemented

--- src/BasicRat.java
+++ src/BasicRat.java
@@ -4,14 +4,48 @@ public class BasicRat extends Rat {
 	private int timeToGrowth; 
 	private int numChildren;
 	private ratTypes gender;
+	public ratTypes getGender() {
+		return gender;
+	}
+	public void setGender(ratTypes gender) {
+		this.gender = gender;
+	}
+	public boolean isBaby() {
+		return isBaby;
+	}
+	private void setIsBaby(boolean isBaby) {
+		this.isBaby = isBaby;
+	}
+	private int getTimeToGrowth() {
+		return timeToGrowth;
+	}
+	private void setTimeToGrowth(int timeToGrowth) {
+		this.timeToGrowth = timeToGrowth;
+	}
+	private void setNumChildren(int numChildren) {
+		this.numChildren = numChildren;
+	}
+	public int getNumChildren() {
+		return numChildren;
+	}
 	public BasicRat(ratTypes type) {
 		super(type);
 		switch (type) {
 		case BABY:
+			int minGrowthTime = 1000;
+			int growthMultiplier = 4000;
+			timeToGrowth = (int) (Math.random() * growthMultiplier) + minGrowthTime; //ms to growth (min 1000, max 5000)
+			setTimeToGrowth(timeToGrowth);
+			setIsBaby(true);
 			break;
 		case FEMALE:
+			setIsBaby(false);
+			setGender(ratTypes.FEMALE);
+			setNumChildren(0);
 			break;
 		default:
+			setIsBaby(false);
+			setGender(ratTypes.MALE);
 			break;
 		}			
 	}
--- src/DeathRat.java
+++ src/DeathRat.java
@@ -1,15 +1,23 @@
 package application;
 
-public class DeathRat {
+public class DeathRat extends Rat {
 	private int killCount;
-	public DeathRat() {
-		
+	private void setKillCount(int killCount) {
+		this.killCount = killCount;
 	}
-	public void checkKillCount() {
-			
-	}	
-	public boolean move() {
-		
+	public DeathRat(ratTypes type) {
+		super(type);
+		setKillCount(0);
+	}
+    public boolean move() {
+		if (killCount < 5) {
+			super.move();
+			//check if rat on current tile
+			//send kill request(s) for rat and increase killcount
+			//needs to only send kill requests if they will not increase the killcount above 5
+		} else {
+			//send kill request for death rat
+		}
 		return true;
 	}
 }
