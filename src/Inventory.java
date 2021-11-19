36404fe7efa109a298a1dd32260bc0c14807d673
JoDer123 <93014538+JoDer123@users.noreply.github.com>
2021-11-19 15:24:12 +0000

    Inventory
    Create my own iteration of the Inventory File

---
+++ src/Inventory.java
@@ -0,0 +1,23 @@
+Public class Inventory extends Entity {
+	private itemsLeft int[];
+	
+	public Inventory () {
+		//Constructor
+	}
+	
+	public String toString () {
+		string result = "The inventory:\n ";
+		//gets information from the "ItemsLeft" Attribute
+		//for (int i = 0; i< size.itemsLeft[] + 1; i++;
+		//  result += itemsLeft[i];
+		return result
+	}
+	
+	public void restock() {
+		//restock
+	}
+	
+	public void placeItem(int item) {
+		//itemsLeft.add(item); ???
+	}
+	
