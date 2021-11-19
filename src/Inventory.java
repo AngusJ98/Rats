4ad8fa8970eca7add7039bf1bba8a8d0b96b77ea
jclmnop <jcheshire@protonmail.com>
2021-11-19 15:49:52 +0000

    fix constructor + private int[] itemsLeft

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,23 +1,27 @@
-Public class Inventory extends Entity {
-	private itemsLeft int[];
-	
-	public Inventory () {
-		//Constructor
-	}
-	
-	public String toString () {
-		String result = "The inventory:\n ";
-		//gets information from the "ItemsLeft" Attribute
-		//for (int i = 0; i< size.itemsLeft[] + 1; i++;
-		//  result += itemsLeft[i];
-		return result;
-	}
-	
-	public void restock() {
-		//restock
-	}
-	
-	public void placeItem(int item) {
-		//itemsLeft.add(item); ???
-	}
+import javafx.scene.image.Image;
+
+public class Inventory extends Entity {
+    private int[] itemsLeft;
+
+    public Inventory(Image image, CollisionType collisionGroup) {
+        super(image, collisionGroup);
+    }
+
+    public String toString() {
+        String result = "The inventory:\n ";
+        //gets information from the "ItemsLeft" Attribute
+        //for (int i = 0; i< size.itemsLeft[] + 1; i++;
+        //  result += itemsLeft[i];
+        return result;
+    }
+
+    public void restock() {
+        //restock
+    }
+
+    public void placeItem(int item) {
+        //itemsLeft.add(item); ???
+    }
+
+}
 	
