747946d8c4127710d7053d8fd961a73e49d9f70c
Ethan Cuevas <ethanmichael@live.com>
2021-11-28 13:59:19 +0000

    Update Bomb
    added comments from spec for reference

--- src/Bomb.java
+++ src/Bomb.java
@@ -25,3 +25,12 @@ public class Bomb extends Entity{
     }
 }
 
+//From the Spec:
+//Once placed, a bomb will count down from 4 in 1 second increments.
+//Once the bomb reaches zero a powerful explosion will extend vertically and horizontally
+//(not diagonally) until it reaches grass in all directions (the
+//explosion passes through tunnels). The grass prevents
+//the explosion from continuing in a given direction. The
+//explosion will kill any rats on these tiles and will also
+//destroy any other items currently placed on these tiles.
+//Each bomb should display a count down: 4, 3, 2, 1.
