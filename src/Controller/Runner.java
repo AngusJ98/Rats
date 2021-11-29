58ec75ac6f89b6d7601ab4719d36c852f0a26def
Aozin <59066161+Aozin@users.noreply.github.com>
2021-11-29 15:41:11 +0000

    Item placement code

--- src/Controller/Runner.java
+++ src/Controller/Runner.java
@@ -1,18 +1,17 @@
 package Controller;
 import entity.*;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import tiles.*;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.control.Control;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
-import java.util.Stack;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
@@ -20,12 +19,12 @@ import static java.lang.Math.min;
 
 public class Runner {
 
-    @FXML
-    private AnchorPane base;
+    @FXML private AnchorPane base;
+    @FXML private ToggleGroup itemToggle;
     private GridPane board;
     private final double pixelWidth = 800;
     private StackPane[][] stackPaneArray = null; // a list of stackpanes indexed by row and col so we can add children later!
-    private int tilePixelWidth = 1; //Set as default, will be changed later
+    private int tilePixelSize = 1; //Set as default, will be changed later
     private int width = 0; //How many tiles wide the board is
     private int height = 0; //How many tiles high the board is
 
@@ -44,6 +43,7 @@ public class Runner {
     }
 
     public void initialize() {
+
         board = new GridPane();
         board.setHgap(0);
         board.setVgap(0);
@@ -66,7 +66,7 @@ public class Runner {
         Image grass = new Image("file:resources/grassBlock.png");
         Image path = new Image("file:resources/dirtBlock.png");
         Image test = new Image("file:resources/strightTun.png");
-        this.tilePixelWidth = min((int) pixelWidth / this.width, (int) pixelWidth / this.height);//Min statement to account for rectangular board
+        this.tilePixelSize = min((int) pixelWidth / this.width, (int) pixelWidth / this.height);//Min statement to account for rectangular board
 
         for (int y = 0; y < tiles.length; y++) {
             for (int x = 0; x < tiles[y].length; x++) {
@@ -85,8 +85,8 @@ public class Runner {
                         tileType.setImage(test);
                         break;
                 }
-                tileType.setFitHeight(this.tilePixelWidth);
-                tileType.setFitWidth(this.tilePixelWidth);
+                tileType.setFitHeight(this.tilePixelSize);
+                tileType.setFitWidth(this.tilePixelSize);
                 tile.getChildren().add(tileType);
                 board.add(tile, x, y);
             }
@@ -124,21 +124,49 @@ public class Runner {
                 Tile gameTile = tiles[y][x];
                 ImageView pic = new ImageView();
                 pic.setImage(gameTile.getImage());
-                pic.setFitHeight(this.tilePixelWidth);
-                pic.setFitWidth(this.tilePixelWidth);
+                pic.setFitHeight(this.tilePixelSize);
+                pic.setFitWidth(this.tilePixelSize);
                 tile.getChildren().add(pic);
                 board.add(tile, x, y);
+
+                //add a transparent button on top so we can add items
+                Button b = new Button();
+                b.setMaxSize(this.tilePixelSize,this.tilePixelSize);
+                b.setPrefSize(this.tilePixelSize,this.tilePixelSize);
+                b.setStyle(
+                        "    -fx-border-color: transparent;\n" +
+                                "    -fx-border-width: 0;\n" +
+                                "    -fx-background-radius: 0;\n" +
+                                "    -fx-background-color: transparent;\n" +
+                                "    -fx-text-fill: #828282;"
+                );
+                //set button action
+                if (gameTile.getType() == tileTypes.PATH) {
+                    b.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override public void handle(ActionEvent e) {
+                            int buttonX = GridPane.getRowIndex(b.getParent());
+                            int buttonY = GridPane.getColumnIndex(b.getParent());
+                            ToggleButton selectedButton = (ToggleButton)itemToggle.getSelectedToggle();
+                            String itemString = selectedButton.getId();
+                            ItemManager.tryPlace(itemString, buttonX ,buttonY);
+                            //TODO Correct to item placement method
+                        }
+                    });
+                }
+
+                board.add(b, x, y);
             }
         }
     }
+
 
 
     public void drawEntities(Entity[] entities) {
         for (Entity entity : entities) {
             ImageView pic = new ImageView();
             pic.setImage(entity.getImage());
-            pic.setFitHeight(this.tilePixelWidth);
-            pic.setFitWidth(this.tilePixelWidth);
+            pic.setFitHeight(this.tilePixelSize);
+            pic.setFitWidth(this.tilePixelSize);
             int x = entity.getXPosition(); //TODO use correct method here
             int y = entity.getYPosition(); //TODO use correct method here
             StackPane targetTile = this.stackPaneArray[x][y];
@@ -151,7 +179,7 @@ public class Runner {
             StackPane pane = (StackPane) node;
             int size = pane.getChildren().size();
             if (size > 1) {
-                pane.getChildren().remove(1, size);
+                pane.getChildren().remove(2, size); //first 2 are tile and button for item placement
             }
         }
     }
