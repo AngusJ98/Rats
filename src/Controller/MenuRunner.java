dd889f7ab974849f590522747b9f41a367f4de37
Aozin <59066161+Aozin@users.noreply.github.com>
2021-12-01 15:11:57 +0000

    Changing path to be correct for level files

--- src/Controller/MenuRunner.java
+++ src/Controller/MenuRunner.java
@@ -59,12 +59,12 @@ public class MenuRunner {
     }
 
     public void level1(ActionEvent actionEvent) throws Exception {
-        Game.setLevelPath("file:resources/level1.json");
+        Game.setLevelPath("file:levelFiles/testLevel.json");
         this.startGame();
     }
 
     public void level2(ActionEvent actionEvent) throws Exception {
-        Game.setLevelPath("file:resources/level2.json");
+        Game.setLevelPath("file:levelFiles/level2.json");
         this.startGame();
     }
 }
