88794a9426bebd9b6b324b46f4ee8aff613ee029
Aozin <59066161+Aozin@users.noreply.github.com>
2021-12-03 15:34:57 +0000

    Rename to pos

--- src/gameHandler/Pos.java
+++ src/gameHandler/Pos.java
@@ -2,16 +2,16 @@ package gameHandler;
 
 import java.util.Objects;
 
-public class Position {
+public class Pos {
     public int x;
     public int y;
-    public Position(int x, int y) {
+    public Pos(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     public boolean equals(Object o) {
-        Position p = (Position) o;
+        Pos p = (Pos) o;
         return this.x == p.x && this.y == p.y;
     }
 
