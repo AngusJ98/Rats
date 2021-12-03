875fa61ae2b445d7b6c08d8ac376b0ac8e295faa
Aozin <59066161+Aozin@users.noreply.github.com>
2021-12-03 14:36:09 +0000

    Fix to position

--- src/gameHandler/Position.java
+++ src/gameHandler/Position.java
@@ -3,8 +3,8 @@ package gameHandler;
 import java.util.Objects;
 
 public class Position {
-    int x;
-    int y;
+    public int x;
+    public int y;
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
