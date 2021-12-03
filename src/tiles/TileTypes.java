36624ab6631c61137ff616d37950892eb4e0e63c
iwiiiiams <92996246+iwiiiiams@users.noreply.github.com>
2021-12-03 21:51:23 +0000

    Finished Javadoc for TileTypes
    Finished and formatted Javadoc for the TileTypes

--- src/tiles/TileTypes.java
+++ src/tiles/TileTypes.java
@@ -2,14 +2,15 @@
  * <p> 1. File-name: TileTypes.java</p>
  * <p> 2. Creation Date: XX/XX/XX </p>
  * <p> 3. Last modification date: XX/XX/XX </p>
- * <p> 4. Purpose of the program: Define an enum for TileTypes</p>
+ * <p> 4. Purpose of the program: Define an enum for the types of Tiles there are</p>
  *
  * @author ??
  */
+//whoever`s class this is please fill in missing information in Javadoc
 package tiles;
 
 /**
- *  Types of tiles which can be used
+ * Types of tiles which can be used
  */
 public enum TileTypes {
     /**
