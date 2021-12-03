5187fed92c80ffa1058f8442969cf3f64e707bdb
iwiiiiams <92996246+iwiiiiams@users.noreply.github.com>
2021-12-03 21:50:43 +0000

    Finished Javadoc
    Finished Javadoc for RatTypes

--- src/entity/RatTypes.java
+++ src/entity/RatTypes.java
@@ -1,5 +1,32 @@
+/**
+ * <p> 1. File-name: RatTypes.java</p>
+ * <p> 2. Creation Date: XX/XX/XX </p>
+ * <p> 3. Last modification date: XX/XX/XX </p>
+ * <p> 4. Purpose of the program: Define an enum for the types of Rats there are</p>
+ *
+ * @author ??
+ */
+//whoever`s class this is please fill in missing information in Javadoc
 package entity;
 
+/**
+ * Types of Rats which a Rat can be
+ */
 public enum RatTypes {
-    MALE, FEMALE, BABY, DEATH;
+    /**
+     * Male Rat
+     */
+    MALE,
+    /**
+     * Female Rat
+     */
+    FEMALE,
+    /**
+     * Baby Rat
+     */
+    BABY,
+    /**
+     * Death Rat
+     */
+    DEATH;
 }
