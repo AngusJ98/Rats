a861ec1b136a0fe65e6825fc026fec69a882391e
lilhat <tah.fighter@hotmail.co.uk>
2021-12-04 13:39:06 +0000

    Add files via upload

---
+++ src/messageOfTheDay/MessageOfTheDay.java
@@ -0,0 +1,55 @@
+package messageOfTheDay;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MessageOfTheDay {
+
+	private static HttpURLConnection connection;
+	
+	public static void main(String[] args) {
+		BufferedReader reader;
+		String line;
+		StringBuffer responseContent = new StringBuffer();
+		
+		try {
+			URL url = new URL("http://cswebcat.swansea.ac.uk/puzzle");
+			connection = (HttpURLConnection) url.openConnection();
+			
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			
+			int status = connection.getResponseCode();
+			//System.out.println(status);
+			
+			if(status > 299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			} else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			}
+			System.out.println(responseContent.toString());
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+		
+	}
+
+}
