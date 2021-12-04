b21edd11ee225c4ad419fb8b46fa4a5e90aa40fb
AndrewS9044 <2007157@swansea.ac.uk>
2021-12-04 13:35:22 +0000

    old item functionality

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -1,13 +1,20 @@
 package entity;
 
-import gameHandler.Pos;
-import javafx.scene.image.Image;
-
-public abstract class Item extends Entity {
-
-    public Item(Image image, Pos pos) {
-        super(image, EntityType.ITEM, pos);
+public abstract class Item {
+	public abstract void onPlacement() {}	
+	public abstract void tick() {}
+    public void onCollision(Entity target) {
+		if !(target == null) { 
+			switch (target.getEntityType()) {
+			case RAT:
+				ratCollision((Rat) target);
+				break;
+			case ITEM:
+				itemCollision((Item) target)
+				break;
+			}
+		}
     }
-    public abstract void onCollision();
-    //public abstract void activateItem();
+    private abstract void ratCollision(Rat target);
+	public abstract void itemCollision(Item target);
 }
