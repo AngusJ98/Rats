ef3463666227737605ca981e3d87849efdb29147
DylanCarm <dylancarm@gmail.com>
2021-12-05 20:24:23 +0000

    javadoc

--- src/gameHandler/Tuple.java
+++ src/gameHandler/Tuple.java
@@ -1,5 +1,14 @@
 package gameHandler;
 
+/**
+ *
+ <p> 1. File-name: Tuple.java</p>
+ <p> 2. Creation Date: (N/A) </p>
+ <p> 3. Last modification date:</p>
+ <p> 4. Purpose of the program: storing objects of different types together</p>
+ * @author Jonny
+ */
+
 public class Tuple<T1, T2, T3, T4, T5, T6> {
     private final T1 first;
     private final T2 second;
@@ -7,6 +16,13 @@ public class Tuple<T1, T2, T3, T4, T5, T6> {
     private final T4 fourth;
     private final T5 fifth;
     private final T6 sixth;
+
+    /**
+     * Constructor.
+     * <p> side-effects</p>
+     * <p> not referentially transparent</p>
+     * @param pos takes all six tuples.
+     */
 
     public Tuple(T1 first, T2 second, T3 third, T4 fourth,
                  T5 fifth, T6 sixth) {
@@ -18,25 +34,55 @@ public class Tuple<T1, T2, T3, T4, T5, T6> {
         this.sixth = sixth;
     }
 
+    /**
+     * method to get the first tuple
+     * @return frist tuple
+     */
+
     public T1 getFirst() {
         return first;
     }
+
+    /**
+     * method to get the second tuple
+     * @return second tuple
+     */
 
     public T2 getSecond() {
         return second;
     }
 
+    /**
+     * method to get the third tuple
+     * @return third tuple
+     */
+
     public T3 getThird() {
         return third;
     }
+
+    /**
+     * method to get the fourth tuple
+     * @return fourth tuple
+     */
 
     public T4 getFourth() {
         return fourth;
     }
 
+    /**
+     * method to get the fifth tuple
+     * @return fifth tuple
+     */
+
     public T5 getFifth() {
         return fifth;
     }
+
+    /**
+     * method to get the sixth tuple
+     * @return sixth tuple
+     */
 
     public T6 getSixth() {
         return sixth;
