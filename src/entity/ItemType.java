7709d308b4fbc4fe8521affcd88b845aa8967fcd
DylanCarm <dylancarm@gmail.com>
2021-12-05 23:23:45 +0000

    Update ItemType.java

--- src/entity/ItemType.java
+++ src/entity/ItemType.java
@@ -1,5 +1,17 @@
 package entity;
 
+/**
+ * <p> 1. File-name: ItemType.java</p>
+ * <p> 2. Creation Date: 1/12/2021 </p>
+ * <p> 3. Last modification date: 5/12/2021</p>
+ * <p> 4. Purpose of the program: To be able to save the game with no issues</p>
+ *
+ * @author Dylan
+ */
+
+/**
+ * enum to name types
+ */
 
 public enum ItemType {
 
