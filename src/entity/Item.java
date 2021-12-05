6846f6e631b592d1509a28a4c451af90f32afc0d
iwiiiiams <92996246+iwiiiiams@users.noreply.github.com>
2021-12-05 16:34:57 +0000

    Javadoc
    Started on Item
    GasPart complete

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -4,25 +4,38 @@ import gameHandler.Game;
 import gameHandler.Pos;
 import javafx.scene.image.Image;
 
+/**
+ * <p> 1. File-name: Item.java</p>
+ * <p> 2. Creation Date: (N/A) </p>
+ * <p> 3. Last modification date:</p>
+ * <p> 4. Purpose of the program: Abstract class for the items implementation</p>
+ *
+ * @author
+ */
 public abstract class Item extends Entity {
-	
-	public Item(Image image, Pos pos) {
+
+    public Item(Image image, Pos pos) {
         super(image, EntityType.ITEM, pos);
-	}	
-	public abstract void onPlacement();
-	public abstract void tick();
-    public void onCollision(Entity target) {
-		if (!(target == null)) {
-			switch (target.getEntityType()) {
-				case RAT:
-					ratCollision((Rat) target);
-					break;
-				case ITEM:
-					itemCollision((Item) target);
-					break;
-			}
-		}
     }
+
+    public abstract void onPlacement();
+
+    public abstract void tick();
+
+    public void onCollision(Entity target) {
+        if (!(target == null)) {
+            switch (target.getEntityType()) {
+                case RAT:
+                    ratCollision((Rat) target);
+                    break;
+                case ITEM:
+                    itemCollision((Item) target);
+                    break;
+            }
+        }
+    }
+
     protected abstract void ratCollision(Rat target);
-	public abstract void itemCollision(Item target);
+
+    public abstract void itemCollision(Item target);
 }
