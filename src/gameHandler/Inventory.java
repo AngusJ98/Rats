d17b89459d6f8db847e47649c65871499540d11f
iwiiiiams <92996246+iwiiiiams@users.noreply.github.com>
2021-12-05 15:33:54 +0000

    changes to inv return

--- src/gameHandler/Inventory.java
+++ src/gameHandler/Inventory.java
@@ -11,31 +11,31 @@ public class Inventory {
 
     // TODO:
     public static int getBombCount() {
-        return ;
+        return bombCount;
     }
 
     public static int getgasCount() {
-        return 1;
+        return gasCount;
     }
 
     public static int getsterileCount() {
-        return 1;
+        return sterileCount;
     }
 
     public static int getnoEntryCount() {
-        return 1;
+        return noEntryCount;
     }
 
     public static int getmaleCount() {
-        return 1;
+        return maleCount;
     }
 
     public static int getfemaleCount() {
-        return 1;
+        return femaleCount;
     }
 
     public static int getDeathCount() {
-        return 1;
+        return deathCount;
     }
 
     public String toString() {
