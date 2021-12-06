75075799672e4e2446faec37de409a4a750bac8a
Aozin <59066161+Aozin@users.noreply.github.com>
2021-12-06 04:04:59 +0000

    java MAIN

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -16,7 +16,11 @@ public class Main extends Application {
     public static final int LEVEL_COUNT = 6;
     public static Player activePlayer = null;
 
-
+    /**
+     * Starts the game by launching the menu
+     * @param primaryStage the static primary to be used
+     * @throws Exception Exceptional
+     */
     public void start(Stage primaryStage) throws Exception{
         stg = primaryStage;
         primaryStage.setResizable(false);
@@ -28,10 +32,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * LAUNCH DA GAME
+     * @param args ARRRRRG I'm a pirate
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * used for switching between scenes
+     * @param newScene the scene to switch to
+     */
     public static void changeScene(Scene newScene) {
         stg.close();
         Main.stg.setScene(newScene);
